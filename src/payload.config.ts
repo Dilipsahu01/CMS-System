15ef393b47864680dfa0916ad3c455c6b0e6ff77
Dilip Sahu <dilipsahu01022006@gmail.com>
2026-08-18 20:02:19 +0530

    Connection pulling issue resolve

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -54,6 +54,7 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
+      max: process.env.VERCEL ? 1 : 10, // Use 1 connection per serverless function on Vercel
     },
     push: true,
   }),
